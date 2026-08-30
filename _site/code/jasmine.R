8bed4e914f7c090dff24f30352a6739d31078a68
Duc Nguyen <46308688+tanduc307@users.noreply.github.com>
2026-08-31 00:06:37 +0700

    .

---
+++ _site/code/jasmine.R
@@ -0,0 +1,11 @@
+# file script jasmine.R bao gồm các function hỗ trợ công việc xử lý thống kê thường gặp
+# tên function được đặt theo tên một loài hoa cũng là tên của một người bạn tuhocr
+# góp ý cải tiến function https://www.facebook.com/groups/tuhocr | tuhocr.com@gmail.com
+
+
+boxplot_ttest <- function(r1, r2){
+  
+  
+  
+  
+}
